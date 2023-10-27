2f8edcafbdec56c16ac966d2bf4480d0cf81fe55
Mateusz Kozłowicz <69522734+Koziok@users.noreply.github.com>
2023-10-27 14:20:02 +0200

    Update Lab1 - Obiektowo-relacyjne bazy danych.sql

--- Lab1 - Obiektowo-relacyjne bazy danych.sql
+++ Lab1 - Obiektowo-relacyjne bazy danych.sql
@@ -200,6 +200,339 @@ BEGIN
 END;
 
 --ZAD 9
+SET SERVEROUTPUT ON;
+DECLARE
+    TYPE t_miesiace IS
+        TABLE OF VARCHAR2(20);
+    moje_miesiace t_miesiace := t_miesiace();
+BEGIN
+    moje_miesiace.extend(12);
+    moje_miesiace(1) := 'Styczen';
+    moje_miesiace(2) := 'Luty';
+    moje_miesiace(3) := 'Marzec';
+    moje_miesiace(4) := 'Kwiecien';
+    moje_miesiace(5) := 'Maj';
+    moje_miesiace(6) := 'Czerwiec';
+    moje_miesiace(7) := 'Lipiec';
+    moje_miesiace(8) := 'Sierpien';
+    moje_miesiace(9) := 'Wrzesien';
+    moje_miesiace(10) := 'Pazdziernik';
+    moje_miesiace(11) := 'Listopad';
+    moje_miesiace(11) := 'Grudzien';
+    FOR i IN moje_miesiace.first()..moje_miesiace.last() LOOP
+        dbms_output.put_line(moje_miesiace(i));
+    END LOOP;
 
+    moje_miesiace.trim(2);
+    FOR i IN moje_miesiace.first()..moje_miesiace.last() LOOP
+        dbms_output.put_line(moje_miesiace(i));
+    END LOOP;
 
+    moje_miesiace.DELETE(5, 7);
+    dbms_output.put_line('Liczba elementow: ' || moje_miesiace.count());
+    FOR i IN moje_miesiace.first()..moje_miesiace.last() LOOP
+        IF moje_miesiace.EXISTS(i) THEN
+            dbms_output.put_line(moje_miesiace(i));
+        END IF;
+    END LOOP;
+END;
+
+--ZAD 10
+CREATE TYPE jezyki_obce AS
+    VARRAY(10) OF VARCHAR2(20);
+/
+
+CREATE TYPE stypendium AS OBJECT (
+        nazwa  VARCHAR2(50),
+        kraj   VARCHAR2(30),
+        jezyki jezyki_obce
+);
+/
+
+CREATE TABLE stypendia OF stypendium;
+
+INSERT INTO stypendia VALUES (
+    'SOKRATES',
+    'FRANCJA',
+    jezyki_obce('ANGIELSKI', 'FRANCUSKI', 'NIEMIECKI')
+);
+
+INSERT INTO stypendia VALUES (
+    'ERASMUS',
+    'NIEMCY',
+    jezyki_obce('ANGIELSKI', 'NIEMIECKI', 'HISZPANSKI')
+);
+
+SELECT * FROM stypendia;
+
+SELECT s.jezyki FROM stypendia s;
+
+UPDATE stypendia
+SET
+    jezyki = jezyki_obce('ANGIELSKI', 'NIEMIECKI', 'HISZPANSKI', 'FRANCUSKI')
+WHERE
+    nazwa = 'ERASMUS';
+
+CREATE TYPE lista_egzaminow AS
+    TABLE OF VARCHAR2(20);
+/
+
+CREATE TYPE semestr AS OBJECT (
+        numer    NUMBER,
+        egzaminy lista_egzaminow
+);
+/
+
+CREATE TABLE semestry OF semestr
+NESTED TABLE egzaminy STORE AS tab_egzaminy;
+
+INSERT INTO semestry VALUES ( semestr(1,
+                                      lista_egzaminow('MATEMATYKA', 'LOGIKA', 'ALGEBRA')) );
+
+INSERT INTO semestry VALUES ( semestr(2,
+                                      lista_egzaminow('BAZY DANYCH', 'SYSTEMY OPERACYJNE')) );
+
+SELECT s.numer, e.*
+FROM semestry s, TABLE(s.egzaminy) e;
+
+SELECT e.*
+FROM semestry s, TABLE ( s.egzaminy ) e;
+
+SELECT * FROM TABLE ( SELECT s.egzaminy FROM semestry s WHERE numer=1 );
+
+INSERT INTO TABLE ( SELECT s.egzaminy FROM semestry s WHERE numer=2 )
+VALUES ('METODY NUMERYCZNE');
+
+UPDATE TABLE ( SELECT s.egzaminy FROM semestry s WHERE numer=2 ) e
+SET e.column_value = 'SYSTEMY ROZPROSZONE'
+WHERE e.column_value = 'SYSTEMY OPERACYJNE';
+
+DELETE FROM TABLE ( SELECT s.egzaminy FROM semestry s WHERE numer=2 ) e
+WHERE e.column_value = 'BAZY DANYCH';
+
+--ZAD 11
+CREATE TYPE koszyk AS TABLE OF VARCHAR2(20);
+
+CREATE TYPE zakup AS OBJECT (
+    id NUMBER,
+    koszyk_produktow koszyk
+);
+
+CREATE TABLE zakupy OF zakup NESTED TABLE koszyk_produktow STORE AS t_koszyk;
+
+INSERT INTO zakupy VALUES (1, koszyk('mleko', 'jajka'));
+INSERT INTO zakupy VALUES (2, koszyk('woda', 'sok'));
+INSERT INTO zakupy VALUES (3, koszyk('chleb', 'maslo', 'ser', 'szynka'));
+
+SELECT * FROM zakupy;
+
+DELETE FROM zakupy z WHERE (
+    SELECT COUNT(*) FROM TABLE (z.koszyk_produktow) k
+       WHERE k.COLUMN_VALUE = 'woda') > 0;
+
+SELECT * FROM zakupy;
+
+--ZAD 12
+CREATE TYPE instrument AS OBJECT (
+        nazwa  VARCHAR2(20),
+        dzwiek VARCHAR2(20),
+        MEMBER FUNCTION graj RETURN VARCHAR2
+) NOT FINAL;
+
+CREATE TYPE BODY instrument AS
+    MEMBER FUNCTION graj RETURN VARCHAR2 IS
+    BEGIN
+        RETURN dzwiek;
+    END;
+END;
+/
+
+CREATE TYPE instrument_dety UNDER instrument (
+        material VARCHAR2(20),
+        OVERRIDING MEMBER FUNCTION graj RETURN VARCHAR2,
+        MEMBER FUNCTION graj (
+               glosnosc VARCHAR2
+           ) RETURN VARCHAR2
+);
+
+CREATE OR REPLACE TYPE BODY instrument_dety AS OVERRIDING
+    MEMBER FUNCTION graj RETURN VARCHAR2 IS
+    BEGIN
+        RETURN 'dmucham: ' || dzwiek;
+    END;
+
+    MEMBER FUNCTION graj (
+        glosnosc VARCHAR2
+    ) RETURN VARCHAR2 IS
+    BEGIN
+        RETURN glosnosc
+               || ':'
+               || dzwiek;
+    END;
+END;
+/
+
+CREATE TYPE instrument_klawiszowy UNDER instrument (
+        producent VARCHAR2(20),
+        OVERRIDING MEMBER FUNCTION graj RETURN VARCHAR2
+);
+
+CREATE OR REPLACE TYPE BODY instrument_klawiszowy AS OVERRIDING
+    MEMBER FUNCTION graj RETURN VARCHAR2 IS
+    BEGIN
+        RETURN 'stukam w klawisze: ' || dzwiek;
+    END;
+END;
+/
+
+DECLARE
+    tamburyn  instrument := instrument('tamburyn', 'brzdek-brzdek');
+    trabka    instrument_dety := instrument_dety('trabka', 'tra-ta-ta', 'metalowa');
+    fortepian instrument_klawiszowy := instrument_klawiszowy('fortepian', 'pingping', 'steinway');
+BEGIN
+    dbms_output.put_line(tamburyn.graj);
+    dbms_output.put_line(trabka.graj);
+    dbms_output.put_line(trabka.graj('glosno'));
+    dbms_output.put_line(fortepian.graj);
+END;
+
+--ZAD 13
+CREATE TYPE istota AS OBJECT (
+        nazwa VARCHAR2(20),
+        NOT INSTANTIABLE MEMBER FUNCTION poluj (
+               ofiara CHAR
+           ) RETURN CHAR
+) NOT INSTANTIABLE NOT FINAL;
+
+CREATE TYPE lew UNDER istota (
+        liczba_nog NUMBER,
+        OVERRIDING MEMBER FUNCTION poluj (
+               ofiara CHAR
+           ) RETURN CHAR
+);
+
+CREATE OR REPLACE TYPE BODY lew AS OVERRIDING
+    MEMBER FUNCTION poluj (
+        ofiara CHAR
+    ) RETURN CHAR IS
+    BEGIN
+        RETURN 'upolowana ofiara: ' || ofiara;
+    END;
+END;
+
+DECLARE
+    krollew    lew := lew('LEW', 4);
+BEGIN
+    dbms_output.put_line(krollew.poluj('antylopa'));
+END;
+
+--ZAD 14
+DECLARE
+    tamburyn instrument;
+    cymbalki instrument;
+    trabka   instrument_dety;
+    saksofon instrument_dety;
+BEGIN
+    tamburyn := instrument('tamburyn', 'brzdek-brzdek');
+    cymbalki := instrument_dety('cymbalki', 'ding-ding', 'metalowe');
+    trabka := instrument_dety('trabka', 'tra-ta-ta', 'metalowa');
+ -- saksofon := instrument('saksofon','tra-taaaa');
+ -- saksofon := TREAT( instrument('saksofon','tra-taaaa') AS instrument_dety);
+END;
+
+--ZAD 15
+CREATE TABLE instrumenty OF instrument;
+
+INSERT INTO instrumenty VALUES ( instrument('tamburyn','brzdek-brzdek') );
+INSERT INTO instrumenty VALUES ( instrument_dety('trabka','tra-ta-ta','metalowa'));
+
+INSERT INTO instrumenty VALUES ( instrument_klawiszowy('fortepian','pingping','steinway') );
+SELECT i.nazwa, i.graj() FROM instrumenty i;
+
+--ZAD 16
+DROP TABLE PRACOWNICY cascade constraints;
+DROP TABLE ZESPOLY cascade constraints;
+DROP TABLE ETATY cascade constraints;
+
+CREATE TABLE ZESPOLY
+	(ID_ZESP NUMBER(4) CONSTRAINT PK_ZESP PRIMARY KEY,
+	NAZWA VARCHAR2(20),
+	ADRES VARCHAR2(20) );
+
+CREATE TABLE ETATY
+      ( NAZWA VARCHAR2(10) CONSTRAINT PK_ETAT PRIMARY KEY,
+	PLACA_MIN NUMBER(6,2),
+	PLACA_MAX NUMBER(6,2));
+
+CREATE TABLE PRACOWNICY
+       (ID_PRAC NUMBER(6) CONSTRAINT PK_PRAC PRIMARY KEY,
+	NAZWISKO VARCHAR2(15),
+	ETAT VARCHAR2(10) CONSTRAINT FK_ETAT REFERENCES ETATY(NAZWA),
+	ID_SZEFA NUMBER(6) CONSTRAINT FK_ID_SZEFA REFERENCES PRACOWNICY(ID_PRAC),
+	ZATRUDNIONY DATE,
+	PLACA_POD NUMBER(6,2) CONSTRAINT MIN_PLACA_POD CHECK(PLACA_POD>100),
+	PLACA_DOD NUMBER(6,2),
+	ID_ZESP NUMBER(4) CONSTRAINT FK_ID_ZESP REFERENCES ZESPOLY(ID_ZESP));
+
+INSERT INTO ZESPOLY VALUES (10,'ADMINISTRACJA',      'PIOTROWO 3A');
+INSERT INTO ZESPOLY VALUES (20,'SYSTEMY ROZPROSZONE','PIOTROWO 3A');
+INSERT INTO ZESPOLY VALUES (30,'SYSTEMY EKSPERCKIE', 'STRZELECKA 14');
+INSERT INTO ZESPOLY VALUES (40,'ALGORYTMY',          'WLODKOWICA 16');
+INSERT INTO ZESPOLY VALUES (50,'BADANIA OPERACYJNE', 'MIELZYNSKIEGO 30');
+
+INSERT INTO ETATY VALUES ('PROFESOR'  ,800.00,1500.00);
+INSERT INTO ETATY VALUES ('ADIUNKT'   ,510.00, 750.00);
+INSERT INTO ETATY VALUES ('ASYSTENT'  ,300.00, 500.00);
+INSERT INTO ETATY VALUES ('STAZYSTA'  ,150.00, 250.00);
+INSERT INTO ETATY VALUES ('SEKRETARKA',270.00, 450.00);
+INSERT INTO ETATY VALUES ('DYREKTOR' ,1280.00,2100.00);
+ 
+INSERT INTO PRACOWNICY VALUES (100,'WEGLARZ'    ,'DYREKTOR'  ,NULL,to_date('01-01-1968','DD-MM-YYYY'),1730.00,420.50,10);
+INSERT INTO PRACOWNICY VALUES (110,'BLAZEWICZ'  ,'PROFESOR'  ,100 ,to_date('01-05-1973','DD-MM-YYYY'),1350.00,210.00,40);
+INSERT INTO PRACOWNICY VALUES (120,'SLOWINSKI'  ,'PROFESOR'  ,100 ,to_date('01-09-1977','DD-MM-YYYY'),1070.00,  NULL,30);
+INSERT INTO PRACOWNICY VALUES (130,'BRZEZINSKI' ,'PROFESOR'  ,100 ,to_date('01-07-1968','DD-MM-YYYY'), 960.00,  NULL,20);
+INSERT INTO PRACOWNICY VALUES (140,'MORZY'      ,'PROFESOR'  ,130 ,to_date('15-09-1975','DD-MM-YYYY'), 830.00,105.00,20);
+INSERT INTO PRACOWNICY VALUES (150,'KROLIKOWSKI','ADIUNKT'   ,130 ,to_date('01-09-1977','DD-MM-YYYY'), 645.50,  NULL,20);
+INSERT INTO PRACOWNICY VALUES (160,'KOSZLAJDA'  ,'ADIUNKT'   ,130 ,to_date('01-03-1985','DD-MM-YYYY'), 590.00,  NULL,20);
+INSERT INTO PRACOWNICY VALUES (170,'JEZIERSKI'  ,'ASYSTENT'  ,130 ,to_date('01-10-1992','DD-MM-YYYY'), 439.70, 80.50,20);
+INSERT INTO PRACOWNICY VALUES (190,'MATYSIAK'   ,'ASYSTENT'  ,140 ,to_date('01-09-1993','DD-MM-YYYY'), 371.00,  NULL,20);
+INSERT INTO PRACOWNICY VALUES (180,'MAREK'      ,'SEKRETARKA',100 ,to_date('20-02-1985','DD-MM-YYYY'), 410.20,  NULL,10);
+INSERT INTO PRACOWNICY VALUES (200,'ZAKRZEWICZ' ,'STAZYSTA'  ,140 ,to_date('15-07-1994','DD-MM-YYYY'), 208.00,  NULL,30);
+INSERT INTO PRACOWNICY VALUES (210,'BIALY'      ,'STAZYSTA'  ,130 ,to_date('15-10-1993','DD-MM-YYYY'), 250.00,170.60,30);
+INSERT INTO PRACOWNICY VALUES (220,'KONOPKA'    ,'ASYSTENT'  ,110 ,to_date('01-10-1993','DD-MM-YYYY'), 480.00,  NULL,20);
+INSERT INTO PRACOWNICY VALUES (230,'HAPKE'      ,'ASYSTENT'  ,120 ,to_date('01-09-1992','DD-MM-YYYY'), 480.00, 90.00,30);
+
+COMMIT;
+
+CREATE TABLE przedmioty (
+    nazwa      VARCHAR2(50),
+    nauczyciel NUMBER
+        REFERENCES pracownicy ( id_prac )
+);
+
+INSERT INTO PRZEDMIOTY VALUES ('BAZY DANYCH',100);
+INSERT INTO PRZEDMIOTY VALUES ('SYSTEMY OPERACYJNE',100);
+INSERT INTO PRZEDMIOTY VALUES ('PROGRAMOWANIE',110);
+INSERT INTO PRZEDMIOTY VALUES ('SIECI KOMPUTEROWE',110);
+INSERT INTO PRZEDMIOTY VALUES ('BADANIA OPERACYJNE',120);
+INSERT INTO PRZEDMIOTY VALUES ('GRAFIKA KOMPUTEROWA',120);
+INSERT INTO PRZEDMIOTY VALUES ('BAZY DANYCH',130);
+INSERT INTO PRZEDMIOTY VALUES ('SYSTEMY OPERACYJNE',140);
+INSERT INTO PRZEDMIOTY VALUES ('PROGRAMOWANIE',140);
+INSERT INTO PRZEDMIOTY VALUES ('SIECI KOMPUTEROWE',140);
+INSERT INTO PRZEDMIOTY VALUES ('BADANIA OPERACYJNE',150);
+INSERT INTO PRZEDMIOTY VALUES ('GRAFIKA KOMPUTEROWA',150);
+INSERT INTO PRZEDMIOTY VALUES ('BAZY DANYCH',160);
+INSERT INTO PRZEDMIOTY VALUES ('SYSTEMY OPERACYJNE',160);
+INSERT INTO PRZEDMIOTY VALUES ('PROGRAMOWANIE',170);
+INSERT INTO PRZEDMIOTY VALUES ('SIECI KOMPUTEROWE',180);
+INSERT INTO PRZEDMIOTY VALUES ('BADANIA OPERACYJNE',180);
+INSERT INTO PRZEDMIOTY VALUES ('GRAFIKA KOMPUTEROWA',190);
+INSERT INTO PRZEDMIOTY VALUES ('GRAFIKA KOMPUTEROWA',200);
+INSERT INTO PRZEDMIOTY VALUES ('GRAFIKA KOMPUTEROWA',210);
+INSERT INTO PRZEDMIOTY VALUES ('PROGRAMOWANIE',220);
+INSERT INTO PRZEDMIOTY VALUES ('SIECI KOMPUTEROWE',220);
+INSERT INTO PRZEDMIOTY VALUES ('BADANIA OPERACYJNE',230);
+
+--ZAD 17
 
